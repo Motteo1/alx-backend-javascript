8b031f47d7e97d0fd4824de91b4b10ce532cf9c8
Motteo1 <timothykirungu@gmail.com>
2023-03-28 19:58:43 +0000

    data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,7 @@
+export default function get ListStudents() {
+  return [
+    { firstName: 'Guillaume', id: 1, location: 'San Fransisco' },
+    { firstName: 'James', id: 2, location: 'Colombia' },
+    { firstNmae: 'Serena', id: 5, location: 'San Fransisco' },
+  ];
+}
